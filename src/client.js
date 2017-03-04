5365e52a710e65e249bbb57d81e86135c825d9f3
sportzbee <contact@sportzbee.com>
2017-03-04 13:57:47 -0800

    Save

--- src/client.js
+++ src/client.js
@@ -15,7 +15,7 @@ import Posts from "./pages/Posts";
 
 window.fbAsyncInit = function() {
 	FB.init({
-		appId	: '1811101489103714',
+		appId	: '317073972028031',
 		cookie  : true,  // enable cookies to allow the server to access the session
 		xfbml   : true,  // parse social plugins on this page
 		version : 'v2.8', // use graph api version 2.8
